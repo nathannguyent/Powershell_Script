9af75c8818029ec7980962291dd1340e2f188d0b
Nathan Nguyen <nathannguyent@gmail.com>
2024-09-20 09:19:55 -0400

    script

---
+++ FirstScript.ps1
@@ -0,0 +1,15 @@
+﻿write-host "My name is Nathan"
+write-host "My first scripts in Powershell"
+$a = 5
+$b = 6
+$c = $a + $b
+$d = $a - $b
+Write-host $c
+Write-host $d
+
+get childItem -path c: \force 
+
+get-childItem -path $env:ProgramFiles -Recurse -Include *.exe |
+    Where-Object -FilterScript {
+    ($_.LastWriteTime -gt '2020-01-01') -and ($_.Length -ge 100mb) -and ($_.Length -le 200mb)
+}
---
+++ lab2.ps1
@@ -0,0 +1,17 @@
+﻿
+Import-Module activedirectory
+
+$ADOU = Import-csv C:\Users\Administrator\Documents\Lab2.csv
+
+foreach ($ou in $ADou)
+{
+
+$name = $ou.name
+$path = $ou.path
+$protection = [System.Convert]::ToBoolean($ou.protection)
+
+New-ADOrganizationalUnit `
+-Name $name `
+-path $path `
+-protectedFromAccidentalDeletion $protection `
+}
